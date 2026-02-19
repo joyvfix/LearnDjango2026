33fcf869c0b061f657b0e1a9408b85d1d1f26560
joyvfix <ahmed.429030.1@gmail.com>
2026-02-19 23:11:58 +0700

    Add template for app

--- django_project/about/views.py
+++ django_project/about/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request,'about.html')
+    return render(request,'about/about.html')
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def blog2(request):
-    return render (request,'blog.html')
+    return render (request,'blog/blog.html')
